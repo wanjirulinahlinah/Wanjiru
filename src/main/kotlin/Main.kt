ca14afc35ad0482c9f3975db9939659b59b70995
wainainalinahlinah <wainainalinahw@gmail.com>
2023-02-17 16:50:59 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    val text = "akirachix"
+    println(text[1]) //first element
+    println(text[3]) //second element
+    println(text[4]) //fourth element
+
+
+    val name = "linah wanjiru"
+    val age =  "30"
+    var sentence = "Hi,my name is $name and i am $age years old"
+    println(sentence)
+
+    var  txt = "television"
+    println(txt .length)
+
+    var name3 = "mercy njeri"
+    var name4 = "Angera mumo"
+    if (name3.equals(name4)){
+        println("thats me")}else{
+        println("i don't know who is that!")}
+
+
+
+
+}
